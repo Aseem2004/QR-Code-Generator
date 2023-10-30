c028737742c7b46a974421f3d3c394b64d2c2679
Aseem Jain <jainaseem2004@gmail.com>
2023-10-30 18:59:24 +0530

    Created generate button

--- index.js
+++ index.js
@@ -7,6 +7,25 @@ const qrContainer=document.querySelector(".body");
 let s=sizes.value;
 GenerateBtn.addEventListener("click",(e)=>{
     e.preventDefault();
-    if(qrText.value.length)
+    // if(qrText.value.length)
     generateQRCode();
-})
+});
+
+// function generateQRCode(){
+//     new QRcode(qrContainer,{
+//         text:qrText.value,
+//         height:s,
+//         width:s,
+//         colorDark:"#000",
+//         colorLight:"#fff",
+//     });
+// }
+function generateQRCode(){
+    new QRCode(qrContainer, {
+        text:qrText.value,
+        height:s,
+        width:s,
+        colorLight:"#fff",
+        colorDark:"#000",
+    });
+}
